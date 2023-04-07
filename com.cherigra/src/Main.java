3059b897b957b534da60455a7adc18e72188c55e
cherigra <nikitakrasovskij5@gmail.com>
2023-04-07 15:59:52 +0300

    decode chack Norris

--- com.cherigra/src/Main.java
+++ com.cherigra/src/Main.java
@@ -1,7 +1,5 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
         public static void main(String[] args) {
@@ -9,31 +7,67 @@ public class Main {
             System.out.println("Input string:");
             String message = scanner.nextLine();
             scanner.close();
+//
+//            StringBuilder binaryMessage = new StringBuilder();
+//            for (char c : message.toCharArray()) {
+//                String binary = Integer.toBinaryString(c);
+//                binaryMessage.append(String.format("%7s", binary).replace(' ', '0'));
+//            }
+//
+//            StringBuilder encodedMessage = new StringBuilder();
+//            int i = 0;
+//            while (i < binaryMessage.length()) {
+//                char currentBit = binaryMessage.charAt(i);
+//                int count = 1;
+//                while (i + 1 < binaryMessage.length() && binaryMessage.charAt(i + 1) == currentBit) {
+//                    count++;
+//                    i++;
+//                }
+//                i++;
+//
+//                encodedMessage.append(currentBit == '0' ? "00 " : "0 ");
+//                for (int j = 0; j < count; j++) {
+//                    encodedMessage.append("0");
+//                }
+//                encodedMessage.append(" ");
+//            }
+//            System.out.println("The result:");
+//            System.out.println(encodedMessage.toString().trim());
+            StringBuilder sb = new StringBuilder();
 
-            StringBuilder binaryMessage = new StringBuilder();
-            for (char c : message.toCharArray()) {
-                String binary = Integer.toBinaryString(c);
-                binaryMessage.append(String.format("%7s", binary).replace(' ', '0'));
-            }
-
-            StringBuilder encodedMessage = new StringBuilder();
-            int i = 0;
-            while (i < binaryMessage.length()) {
-                char currentBit = binaryMessage.charAt(i);
-                int count = 1;
-                while (i + 1 < binaryMessage.length() && binaryMessage.charAt(i + 1) == currentBit) {
-                    count++;
-                    i++;
+            int whiteSpaceCounter = 0, startingIndex = 0;
+            for (int i = 0; i < message.length(); i++) {
+                if (message.charAt(i) == ' ') {
+                    whiteSpaceCounter++;
                 }
-                i++;
 
-                encodedMessage.append(currentBit == '0' ? "00 " : "0 ");
-                for (int j = 0; j < count; j++) {
-                    encodedMessage.append("0");
+                if (whiteSpaceCounter == 2 || i == message.length() - 1) {
+                    String pair = message.substring(startingIndex, i + 1).trim();
+                    startingIndex = i;
+                    whiteSpaceCounter = 0;
+
+                    String[] pairArr = pair.split(" ");
+                    String oneZero = pairArr[0].equals("0") ? "1" : "0";
+                    sb.append(oneZero.repeat(pairArr[1].length()));
                 }
-                encodedMessage.append(" ");
             }
             System.out.println("The result:");
-            System.out.println(encodedMessage.toString().trim());
+            System.out.println( binaryToString(sb.toString()));
         }
-}
+    private static String binaryToString(String binaryString) {
+        ArrayList<String> lettersInBinary = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        // Split at 7.
+        for (int i = 0; i < binaryString.length(); i += 7) {
+            lettersInBinary.add(binaryString.substring(i, i + 7));
+        }
+
+        // Converts the binary to character.
+        for (String letter : lettersInBinary) {
+            sb.append((char) Integer.parseInt(letter, 2));
+        }
+
+        return sb.toString();
+    }
+        }
